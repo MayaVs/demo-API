359eea62c51ff1ce2fb41eaa118a5f4f9ad7b26b
Maya Vasileva <maya.vassileva@gmail.com>
2021-11-08 21:44:56 +0000

    created drinks API with TTD

---
+++ src/main/java/com/example/demo/controller/CoffeeController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+        import com.example.demo.model.Coffee;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.bind.annotation.RestController;
+
+        import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class CoffeeController {
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping(value = "/coffeelover")
+    public String coffee(){
+        return "I like coffee!";
+    }
+
+    @GetMapping(value = "/coffee")
+    public Coffee coffee(@RequestParam(value = "name", defaultValue =
+            "latte") String name) {
+        return new Coffee(counter.incrementAndGet(), name);
+    }
+}
